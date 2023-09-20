a905c949df63674aa586b6688443840fda19769a
brijeshb9498 <brijeshb9498@gmail.com>
2023-09-20 16:25:45 +0530

    ohj

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_instance" {
     ami = "ami-0688ba7eeeeefe3cd"
-    instance_type = "t2.micro"
-     tags = {
+    instance_type = var.instance_type
+         tags = {
 
         value = "this_is_my_first_creation"
 
---
+++ ec2/variable.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {
+  type        = string
+  default     = ""
+  description = "please provide input"
+}
+
+
+
