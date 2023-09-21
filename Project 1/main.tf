f434ceb12895d9e5111cf4f18dd918bb4bf8c47b
brijeshb9498 <brijeshb9498@gmail.com>
2023-09-21 15:11:18 +0530

    updu

---
+++ Project 1/main.tf
@@ -0,0 +1,5 @@
+module "root_module_create_ec2" {
+        source = ./modules/create_ec2
+        instance_type= var.root_instance_type
+        pem_key= var.root_pem_key
+}
---
+++ Project 1/variable.tf
@@ -0,0 +1,9 @@
+variable "root_instance_type" {
+    type = string
+    default = ""
+}
+
+variable "root_pem_key" {
+    type = string
+    default = ""  
+}
