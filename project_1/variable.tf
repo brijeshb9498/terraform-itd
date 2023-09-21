579c0919a0d637a324d53a43019493a5f2e9aa08
brijeshb9498 <brijeshb9498@gmail.com>
2023-09-21 15:23:15 +0530

    B

--- project_1/variable.tf
+++ project_1/variable.tf
@@ -1,9 +1,7 @@
 variable "root_instance_type" {
-    type = string
-    default = ""
+         type = string
 }
 
 variable "root_pem_key" {
-    type = string
-    default = ""  
+         type = string 
 }
