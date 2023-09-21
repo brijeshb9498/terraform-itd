d00014c1bed703863c0022cbef5cc8158ae7bb3b
brijeshb9498 <brijeshb9498@gmail.com>
2023-09-21 16:21:27 +0530

    B

--- project_1/variable.tf
+++ project_1/variable.tf
@@ -5,3 +5,12 @@ variable "root_instance_type" {
 variable "root_pem_key" {
          type = string 
 }
+
+variable "root_bucket_name" {
+  type        = string
+}
+
+variable "root_sse_algorithm_type" {
+  type        = string
+}
+
