9cc01a5d3d763c91b41c2229b368686ff5d6fb61
brijeshb9498 <brijeshb9498@gmail.com>
2023-09-21 16:13:51 +0530

    B

--- project_1/main.tf
+++ project_1/main.tf
@@ -3,3 +3,10 @@ module "root_module_create_ec2" {
         instance_type= var.root_instance_type
         pem_key= var.root_pem_key
 }
+
+module "root_module_create_s3" {
+  source = "./modules/create_s3"
+  bucket_name= var.root_bucket_name
+  sse_algorithm_type= var.root_sse_algorithm_type
+  
+}
---
+++ project_1/modules/create_ec2/create_s3/variable.tf
@@ -0,0 +1,8 @@
+variable "bucket_name" {
+  type        = string
+}
+
+variable "sse_algorithm_type" {
+  type        = string
+}
+
