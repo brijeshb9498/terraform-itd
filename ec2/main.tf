edaaa2cfeb5e12099a75a794d820d7fbacb5349c
Ubuntu <ubuntu@ip-172-31-46-227.ap-south-1.compute.internal>
2023-09-21 08:53:55 +0000

    update

--- ec2/main.tf
+++ ec2/main.tf
@@ -10,7 +10,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "my_instance" {
-    ami = data.aws_ami.ubuntu
+    ami = data.aws_ami.ubuntu.id
     instance_type = var.instance_type
          tags = {
 
